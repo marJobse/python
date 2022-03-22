283a4b4d59f5cac47036f335f8f4c2ea3a9b9e1c
marJobse <102001123+marJobse@users.noreply.github.com>
2022-03-22 14:11:59 -0300

    Update funcionNroParamVariables.py

--- funcionNroParamVariables.py
+++ funcionNroParamVariables.py
@@ -58,3 +58,24 @@ imprimo_elementos1("I", "II", "III", "IV")
 print("-" * 20)
 print("Invoco a imprimo_elementos2 con parámetros simples")
 imprimo_elementos2(1,2,3,4)
+
+def imprimo_muchos_valores(mensaje_inicial, *en_otro_idioma, **en_detalle):
+  print("Mensaje original")
+  print(mensaje_inicial)
+  print("\nEn otros idiomas")
+  print("-" * 40)
+  for val in en_otro_idioma:
+    print(val)
+    print("\nEn detalle")
+    print("-" * 40)
+  for clave in en_detalle:
+    print(f"{clave}: {en_detalle[clave]}")
+    print("\nFuente: traductor de Google. ")
+  
+imprimo_muchos_valores("Hola",
+"hello", "Hallo", "Aloha ", "Witam", "Kia ora",
+ingles= "hello",
+aleman="Hallo",
+hawaiano="Aloha",
+polaco="Witam",
+maori="Kia ora")
